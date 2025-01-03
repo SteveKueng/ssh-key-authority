10519f18edfcb2973269c07f2962b501f10cbb7e
Steve Küng <steve.kueng@gmail.com>
2025-01-03 13:55:50 +0100

    update docker image to php

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,35 +1,14 @@
-#!/usr/bin/env ash
-if [ `whoami` == 'keys-sync' ]; then
-  if [ ! -r /ska/config/config.ini ]; then
-      echo "config.ini not found or incorrect permissions."
-      echo "Permissions must be $(id -u keys-sync):$(id -g keys-sync) with at least 400"
-      exit 1
-  fi
-  if [ ! -r /ska/config/keys-sync ]; then
-      echo "private key not found or incorrect permissions."
-      echo "Permissions must be $(id -u keys-sync):$(id -g keys-sync) with 400"
-      exit 1
-  fi
-  if [ ! -r /ska/config/keys-sync.pub ]; then
-      echo "public key not found or incorrect permissions."
-      echo "Permissions must be $(id -u keys-sync):$(id -g keys-sync) with at least 400"
-      exit 1
-  fi
-  if ! grep "^timeout_util = BusyBox$" /ska/config/config.ini > /dev/null; then
-      echo "timeout_util must be set to BusyBox."
-      echo "Change it to: timeout_util = BusyBox"
-      exit 1
-  fi
-elif [ $(id -u) = 0 ]; then
-  if ! sudo -u keys-sync /entrypoint.sh; then
-    exit 1
-  fi
-  rsync -a --delete /ska/public_html/ /public_html/
-  /usr/sbin/crond
-  echo "Waiting for database..."
-  sleep 5
-  /ska/scripts/syncd.php --user keys-sync
-  /usr/sbin/php-fpm7 -F
-else
-  echo "Must be executed with root"
-fi
+#!/usr/bin/env bash
+
+export LDAP_HOST=$(awk -F "= " '/host = / {print $2}' /ska/config/config.ini | sed 's/"//g')
+export LDAP_BIND_DN=$(awk -F "= " '/bind_dn = / {print $2}' /ska/config/config.ini | sed 's/"//g')
+export LDAP_BIND_PW=$(awk -F "= " '/bind_password = / {print $2}' /ska/config/config.ini | sed 's/"//g')
+export LDAP_DN_USER=$(awk -F "= " '/dn_user = / {print $2}' /ska/config/config.ini | sed 's/"//g')
+
+# start cron
+service cron start
+
+echo "Waiting for database..."
+sleep 5
+/ska/scripts/syncd.php --user keys-sync
+apache2-foreground
