c198a711296cf2a4120a0e02d46b0d22c240a58c
Steve Küng <steve.kueng@gmail.com>
2025-01-05 14:16:54 +0100

    update cron

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -6,9 +6,26 @@ export LDAP_BIND_PW=$(awk -F "= " '/bind_password = / {print $2}' /ska/config/co
 export LDAP_DN_USER=$(awk -F "= " '/dn_user = / {print $2}' /ska/config/config.ini | sed 's/"//g')
 
 # start cron
+echo "Starting cron..."
 service cron start
 
+# set permissions
+echo "Setting permissions..."
+chown -R key-sync /ska/config/key-sync*
+chmod 600 /ska/config/key-sync*
+
+# wait for database
 echo "Waiting for database..."
 sleep 5
+
+# sync keys
+echo "Syncing keys..."
 /ska/scripts/syncd.php --user keys-sync
+
+# show logs
+echo "Showing logs..."
+tail -f /var/log/keys/sync.log &
+
+# start apache
+echo "Starting apache..."
 apache2-foreground
